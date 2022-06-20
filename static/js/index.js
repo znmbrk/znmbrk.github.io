2ba542a616239ffe47e35186cf504de7761c36ec
znmbrk <zain.mobarik@challoners.org>
2022-06-20 04:58:03 +0100

    Adding my actual eperience
    Adding all my projects and a description of them as well as messing around with some javascript animations.

--- static/js/index.js
+++ static/js/index.js
@@ -10,3 +10,44 @@ navLinks.forEach(link => {
         document.body.classList.remove("nav-open");
     })
 })
+
+ScrollReveal({
+    reset: true,
+    distance: "60px",
+    duration: 2500,
+    delay: 400
+});
+
+ScrollReveal().reveal(".about-me__img");
+ScrollReveal().reveal("intro__img");
+ScrollReveal().reveal('.e', { delay: 150 });
+
+/*
+$(document).ready(function() {
+    $(".explode").click(function() {
+        $(".logo__img").explode({
+            omitLastLine: false,
+            radius: 80,
+            minRadius: 20,
+            release: true,
+            fadeTime:300,
+            recycle:true,
+            recycleDelay:500,
+            fill:true,
+            explodeTime:300,
+            maxAngle:360,
+            gravity:0,
+            round:false,
+            groundDistance:400,
+            ignoreCompelete:false,
+            land:true,
+            checkOutBound,
+            finish
+        });
+    });
+});
+*/
+
+
+
+
